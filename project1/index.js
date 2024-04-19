c75f6c3ff8727b8a4ddc87368b6ba01dcf2387a6
akib <sayyedakib104@gmail.com>
2024-04-19 15:28:20 +0530

    Updated

--- project1/index.js
+++ project1/index.js
@@ -1,1 +1,10 @@
-console.log("Akib sayyed");
+
+var express = require("express");
+// console.log(typeof express);
+
+
+var app = express();
+app.get("/user",(req, res)=>{
+    res.send("Data From Database")
+});
+app.listen(9000);
--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-//HTTP is the bulled in module in node.js which will helps us to create custome server = Live server
-
-var server = http.createServer((req, res)=>{
-res.write("Is Respons");
-res.end();
-}) //request is used to capture incoming data & Response Sending the data from Node of Frontend Server
-server.listen(1999); 
