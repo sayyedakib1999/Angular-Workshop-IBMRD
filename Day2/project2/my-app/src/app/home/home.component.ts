0f7c806297923f1ef13736ef6cb8ff825e20d85d
akib <sayyedakib104@gmail.com>
2024-04-20 16:05:59 +0530

    Updated

--- Day2/project2/my-app/src/app/home/home.component.ts
+++ Day2/project2/my-app/src/app/home/home.component.ts
@@ -1,21 +1,25 @@
 import { Component } from '@angular/core';
 import { FetchdataService } from '../fetchdata.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   constructor(private x1:FetchdataService) { 
   }
+  //ngOnInit() is the Cycle to call the API's
+
+  public record:any;
   ngOnInit():void {
     console.log('Fetch node js data from');
     this.x1.apiFromnodejs().subscribe((result)=>{
       console.log(result);
-
+      this.record = result
     });
   }
 
