0f70443605d5a914a5bf5b92b8b534f3d53f8c02
akib <sayyedakib104@gmail.com>
2024-04-20 14:47:05 +0530

    updated

--- Day2/project2/my-app/src/app/app.config.ts
+++ Day2/project2/my-app/src/app/app.config.ts
@@ -3,7 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideHttpClient()]
 };
+function provideClient(): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
+  throw new Error('Function not implemented.');
+}
+
